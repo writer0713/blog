6409d32ca2eeff8a69a3d7c6b70f3b3c71424168
KIM JUNGHWAN <writer0713@naver.com>
2019-02-14 20:17:05 +0900

    Post 객체 변경
    - author 디폴트값 추가
    - 생성자 추가
    - url 추가

--- src/main/java/com/writer0713/blog/Model/Post.java
+++ src/main/java/com/writer0713/blog/Model/Post.java
@@ -6,9 +6,21 @@ import lombok.Data;
 public class Post {
 
 	private String title;
-	private String author;
+	private String author = "writer0713";
 	private String date;
 	private String content;
 	private String url;
 
+	public Post(String title, String date, String content) {
+		this.title = title;
+		this.date = date;
+		this.content = content;
+	}
+
+	public Post(String title, String date, String content, String postNo) {
+		this.title = title;
+		this.date = date;
+		this.content = content;
+		this.url = "/post/".concat(postNo);
+	}
 }
