21524d2560a60d6a33d6d41fac24e8f2850c03dc
KIM JUNGHWAN <writer0713@naver.com>
2019-02-14 19:39:52 +0900

    VO 객체 lombok 사용하도록 수정
    - getter, setter, toString, equals 등 메소드 다 지우고 롬복으로 대체

--- src/main/java/com/writer0713/blog/Model/Post.java
+++ src/main/java/com/writer0713/blog/Model/Post.java
@@ -1,5 +1,8 @@
 package com.writer0713.blog.Model;
 
+import lombok.Data;
+
+@Data
 public class Post {
 
 	private String title;
@@ -8,54 +11,4 @@ public class Post {
 	private String content;
 	private String url;
 
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	@Override
-	public String toString() {
-		return "Post{" +
-				"title='" + title + '\'' +
-				", author='" + author + '\'' +
-				", date='" + date + '\'' +
-				", content='" + content + '\'' +
-				", url='" + url + '\'' +
-				'}';
-	}
 }
