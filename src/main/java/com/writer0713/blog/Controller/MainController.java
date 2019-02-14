cc3dbcbe1cca523cffe120bfe3a1abef3ff9bc21
KIM JUNGHWAN <writer0713@naver.com>
2019-02-14 20:18:09 +0900

    post detail 페이지 추가
    - post를 선택하면 선택한 포스트 페이지로 넘어가는 로직 추가

--- src/main/java/com/writer0713/blog/Controller/MainController.java
+++ src/main/java/com/writer0713/blog/Controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -27,8 +28,17 @@ public class MainController {
 		return "index";
 	}
 
-	@GetMapping("/post")
-	public String post() {
+	@GetMapping("/post/{no}")
+	public String post(@PathVariable String no, Model model) {
+
+		// TODO 404 페이지로 리다이렉트
+		if (no == null) {
+		}
+
+		Post post = crawlService.getPostBy(no);
+
+		model.addAttribute("post", post);
+
 		return "post";
 	}
 
--- src/main/java/com/writer0713/blog/Service/CrawlService.java
+++ src/main/java/com/writer0713/blog/Service/CrawlService.java
@@ -3,22 +3,25 @@ package com.writer0713.blog.Service;
 import com.writer0713.blog.Model.Post;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class CrawlService {
 
-	private static final String BLOG_URL = "https://blog.naver.com/PostList.nhn?from=postList&blogId=writer0713&categoryNo=0&currentPage=";
+	private static final String URL_FOR_POSTS = "https://blog.naver.com/PostList.nhn?from=postList&blogId=writer0713&categoryNo=0&currentPage=";
+	private static final String URL_FOR_ONE_POST = "https://blog.naver.com/PostView.nhn?blogId=writer0713&logNo=";
 
 	public List<Post> getPostsBy(int pageNo) {
 
 		String pageNoStr = String.valueOf(pageNo);
-		String requestURL = BLOG_URL.concat(pageNoStr);
+		String requestURL = URL_FOR_POSTS.concat(pageNoStr);
 
 		Document doc = this.getDocument(requestURL);
 		Elements postElements = this.getPosts(doc);
@@ -28,18 +31,31 @@ public class CrawlService {
 			String date = element.select("span.se_publishDate").text();
 			String content = element.select("div.se-main-container").text()
 					.substring(0, 200).concat("...");
-			String author = "writer0713";
+			String url = element.select("a.url").attr("title");
+			String postNo = Stream.of(url.split("/")).reduce((first, last) -> last).get(); // get last element after split
 
-			Post post = new Post();
-			post.setTitle(title);
-			post.setDate(date);
-			post.setContent(content);
-			post.setAuthor(author);
+			Post post = new Post(title, date, content, postNo);
 
 			return post;
 		}).collect(Collectors.toList());
 
 		return posts;
+	}
+
+	public Post getPostBy(String no) {
+
+		String requestURL = URL_FOR_ONE_POST.concat(no);
+
+		Document doc = getDocument(requestURL);
+		Element postElement = doc.select("div#post_1").first();
+
+		String title = postElement.select("div.se-title-text").text();
+		String date = postElement.select("span.se_publishDate").text();
+		String content = postElement.select("div.se-main-container").html();
+
+		Post post = new Post(title, date, content);
+
+		return post;
 	}
 
 	private Document getDocument(String requestURL) {
