6e9365d44bbf27d78f5f8dc4798af4c698667b42
KIM JUNGHWAN <writer0713@naver.com>
2019-05-09 19:57:21 +0900

    #25 service-worker.js register 로직 수정

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/service-worker.js')
+    .register('/js/service-worker.js')
     .then(function(registration) {
       console.log(
         'Service Worker registration successful with scope: ',
